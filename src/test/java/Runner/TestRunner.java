1d4e1b0e67a719c59ddf96b324c59637440d6b29
pranjal-13 <69795266+pranjal-13@users.noreply.github.com>
2021-02-23 19:11:12 +0530

    Update TestRunner.java

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		,glue= {"StepDefinition"}
 		,monochrome=true
 		,dryRun=false	
-		,tags="@TC_15_09_OnusPaymentflowwithExceptionfromQueuedToPosting"
+		,tags="@TC_09_01_AddUserScenario"
 		,plugin = {"pretty", "html:target/cucumber-html-report",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 
